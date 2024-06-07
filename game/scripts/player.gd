0111fdf74508e3785c5f9ed4fa7548a2785fba40
Christopher Perkins <Christopher.Perkins@null.net>
2024-06-07 15:50:08 -0400

    feat: Basic Player Created

---
+++ game/scripts/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+const BOOST_VELOCITY = 500.0
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if !is_on_floor():
+		animated_sprite.play('jump');
+	else:
+		if direction:
+			animated_sprite.flip_h = direction == -1
+			animated_sprite.play('run');
+		else:
+			animated_sprite.play('idle');
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
