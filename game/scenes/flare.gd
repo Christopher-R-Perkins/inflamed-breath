712498e43f2e8c7156591f30be0b32da8fa16479
Christopher Perkins <Christopher.Perkins@null.net>
2024-06-07 19:28:40 -0400

    feat: Early Flare Behavior added

---
+++ game/scenes/flare.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+@export_flags_2d_physics var mask
+
+const RAY_CASTS = [
+	Vector2(1, 0), 
+	Vector2(.707, .707), 
+	Vector2(0, 1), 
+	Vector2(-.707, .707), 
+	Vector2(-1, 0), 
+	Vector2(-.707,-.707), 
+	Vector2(0, -.707), 
+	Vector2(.707, -.707),
+]
+const BLAST_RADIUS = 12
+
+signal boosted
+@onready var animator = $AnimationPlayer
+
+enum Flare_State {IDLE, FIRING}
+@export var state = Flare_State.IDLE
+
+var checks = 0
+var checkTime = 0
+
+func fire():
+	if state == Flare_State.FIRING:
+		return
+	
+	animator.play('fire')
+	checks = 0
+	checkTime = 0
+	
+	
+func _process(delta: float) -> void:
+	if state == Flare_State.IDLE || checks > 3:
+		return
+		
+	checkTime -= delta
+	if checkTime >= 0:
+		return
+		
+	checkTime = .1
+	checks += 1
+		
+	var space_state = get_world_2d().direct_space_state
+	
+	var get_hits = func(acc, ray):
+		var query = PhysicsRayQueryParameters2D.create(
+			global_position + (ray * 4), global_position - (ray * BLAST_RADIUS), mask)
+		var result = space_state.intersect_ray(query)
+		
+		if result:
+			return acc + [global_position - result.position]
+		else:
+			return acc
+		
+	var hits = RAY_CASTS.reduce(get_hits, [])
+	
+	var direction_of_blast = hits.reduce(func(acc, a): return acc + a, Vector2(0 , 0)).normalized()
+	if direction_of_blast.y > 0:
+		return
+		
+	checks = 1000
+	
+	emit_signal('boosted', direction_of_blast)
--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,19 +1,29 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-const BOOST_VELOCITY = 500.0
+const DRAG = .1
+const SPEED = 75.0
+const JUMP_VELOCITY = -150.0
+const BOOST_VELOCITY = 300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var flare = $Flare
+
+var boosted = false;
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif boosted:
+		velocity = Vector2(0, 0)
+		boosted = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_pressed('ui_cancel'):
+		flare.fire()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,8 +39,21 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play('idle');
 	
 	if direction:
-		velocity.x = direction * SPEED
+		if SPEED >= abs(velocity.x):
+			velocity.x = direction * SPEED
+		elif boosted && direction != velocity.x / abs(velocity.x):
+			velocity.x += direction * SPEED * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DRAG if boosted else SPEED)
 
 	move_and_slide()
+
+
+func _on_flare_boosted(blast_direction) -> void:
+	if blast_direction == Vector2(0, 0):
+		return
+		
+	if not is_on_floor():
+		velocity = BOOST_VELOCITY * blast_direction
+		boosted = true
+	pass # Replace with function body.
